6b18c276f336606132954332e6fd6f790b9dcd78
ajayy4007 <ajayy4007@gmail.com>
2023-05-25 10:25:41 +0530

    Creates StrategyDesignPatter using Spring

--- src/com/nt/text/StratgeyDpTest.java
+++ src/com/nt/text/StratgeyDpTest.java
@@ -1,13 +1,17 @@
 package com.nt.text;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
 import com.nt.comp.FlipKart;
-import com.nt.factory.FlipKartFactory;
 public class StratgeyDpTest {
 	public static void main(String[] args) {
-		//Target class having Dependent class obj
-		FlipKart fpkt=FlipKartFactory.getInstance();
-		//invoke b.method
-		String result=fpkt.shopping(new String[] {"shirt","trouser","watch"},
-			new float [] {4000.0f,6000.0f,30000.0f});
+		//Create IOC container 
+		DefaultListableBeanFactory factory=new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
+		//get Target Class Object
+		FlipKart fpkt=factory.getBean("fpkt",FlipKart.class);
+		String result=fpkt.shopping(new String[] {"mangaoes","tamrind","suger canes"}, new float[] {900.0f,300.0f,100.0f});
 		System.out.println(result);
 	}
 }
